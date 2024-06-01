6500de28b14f421909b2e79a2cfcf7ed89109317
GaboSnipe <gabriel.gulua@geolab.edu.ge>
2024-06-02 01:02:39 +0400

    fix

--- models/Blacklist.js
+++ models/Blacklist.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const blackListSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  reason: { type: String, required: true },
+  reason: { type: String, },
 });
 
 const BlacklistModel = mongoose.model('Blacklist', blackListSchema);
